ac647b79f4fb80d61f705c30ff3ca1df21194dc3
Philip Rehberger <me@philiprehberger.com>
2026-04-03 15:50:04 -0600

    Add primary barrel file matching package name

--- lib/collection_ext.dart
+++ lib/collection_ext.dart
@@ -1,5 +1,1 @@
-/// Iterable and Map extensions for groupBy, chunk, zip, distinct, and more.
-library;
-
-export 'src/iterable_extensions.dart';
-export 'src/map_extensions.dart';
+export "philiprehberger_collection_ext.dart";
---
+++ lib/philiprehberger_collection_ext.dart
@@ -0,0 +1,5 @@
+/// Iterable and Map extensions for groupBy, chunk, zip, distinct, and more.
+library;
+
+export 'src/iterable_extensions.dart';
+export 'src/map_extensions.dart';
